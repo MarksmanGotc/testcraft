331fc96397c6e584bd9d02b17760f747f47bcec1
MarksmanGotc <151929315+MarksmanGotc@users.noreply.github.com>
2025-06-06 15:21:51 +0300

    Update products.js

--- products.js
+++ products.js
@@ -1,276 +1,389 @@
-let craftItem = {
-  "products": [
-    {
-      "name": "Wooden Bucket",
-      "level": 1,
-      "materials": {"weirwood": 10240, "kingswood-oak": 10240},
-      "img": "item/icon_eq_standard_helmet_woodenbucket.webp"
-    },
-    {
-      "name": "Leather Vest",
-      "level": 1,
-      "materials": {"hide": 10240, "leather-straps": 10240},
-      "img": "item/icon_eq_standard_chest_leathervest.webp"
-    },
-    {
-      "name": "Flax Legwraps",
-      "level": 1,
-      "materials": {"milk-of-the-poppy": 10240, "goldenheart-wood": 10240},
-      "img": "item/icon_eq_standard_pants_flaxlegwraps.webp"
-    },
-	{
-      "name": "Straw Sandals",
-      "level": 1,
-      "materials": {"hide": 10240, "leather-straps": 10240},
-      "img": "item/icon_eq_standard_boots_strawsandals.webp"
-    },
-    {
-      "name": "Copper Band",
-      "level": 1,
-      "materials": {"black-iron": 10240, "copper-bar": 10240},
-      "img": "item/icon_eq_standard_ring_copperband.webp"
-    },
-    {
-      "name": "Dagger",
-      "level": 1,
-      "materials": {"ironwood": 10240, "kingswood-oak": 10240},
-      "img": "item/icon_eq_standard_weapon_shortsword.webp"
-    },
-	
-	{
-      "name": "CTW Helm",
-      "level": 1,
-      "materials": {"silk": 6144, "black-iron": 6144, "copper-bar": 6144},
-      "img": "item/ctw-head.png",
-	  "warlord": true
-    },
-	{
-      "name": "CTW Chestplate",
-      "level": 1,
-      "materials": {"milk-of-the-poppy": 6144, "hide": 6144, "ironwood": 6144},
-      "img": "item/ctw-chest.png",
-	  "warlord": true
-    },
-	{
-      "name": "CTW Pants",
-      "level": 1,
-      "materials": {"kingswood-oak": 6144, "wildfire": 6144, "hide": 6144},
-      "img": "item/ctw-pants.png",
-	  "warlord": true
-    },
-	{
-      "name": "CTW Boots",
-      "level": 1,
-      "materials": {"ironwood": 6144, "goldenheart-wood": 6144, "wildfire": 6144},
-      "img": "item/ctw-boots.png",
-	  "warlord": true
-    },
-	{
-      "name": "CTW Ring",
-      "level": 1,
-      "materials": {"weirwood": 6144, "leather-straps": 6144, "black-iron": 6144},
-      "img": "item/ctw-ring.png",
-	  "warlord": true
-    },
-	{
-      "name": "CTW Sword",
-      "level": 1,
-      "materials": {"ironwood": 6144, "goldenheart-wood": 6144, "wildfire": 6144},
-      "img": "item/ctw-weapon.png",
-	  "warlord": true
-    },
-	
-    {
-      "name": "Wool Bandana",
-      "level": 5,
-      "materials": {"kingswood-oak": 15360, "milk-of-the-poppy": 15360},
-      "img": "item/icon_eq_standard_helmet_woolbandana.webp"
-    },
-    {
-      "name": "Boiled Leather Shirt",
-      "level": 5,
-      "materials": {"hide": 15360, "dragonglass": 15360},
-      "img": "item/icon_eq_standard_chest_boiledleathershirt.webp"
-    },
-    {
-      "name": "Leather Poleyns",
-      "level": 5,
-      "materials": {"hide": 15360, "leather-straps": 15360},
-      "img": "item/icon_eq_standard_pants_leatherpoleyns.webp"
-    },
-    {
-      "name": "Leather Booties",
-      "level": 5,
-      "materials": {"hide": 10240, "silk": 10240, "weirwood": 10240},
-      "img": "item/icon_eq_standard_boots_leatherbooties.webp"
-    },
-    {
-      "name": "Bronze Band",
-      "level": 5,
-      "materials": {"copper-bar": 15360, "milk-of-the-poppy": 15360},
-      "img": "item/icon_eq_standard_ring_bronzeband.webp"
-    },
-    {
-      "name": "Spear",
-      "level": 5,
-      "materials": {"leather-straps": 15360, "wildfire": 15360},
-      "img": "item/icon_eq_standard_weapon_spear.webp"
-    },	
-	{
-      "name": "CTW Helm",
-      "level": 5,
-      "materials": {"kingswood-oak": 10240, "wildfire": 10240, "hide": 10240},
-      "img": "item/ctw-head.png",
-	  "warlord": true
-    },
-	{
-      "name": "CTW Chestplate",
-      "level": 5,
-      "materials": {"silk": 10240, "black-iron": 10240, "copper-bar": 10240},
-      "img": "item/ctw-chest.png",
-	  "warlord": true
-    },
-	{
-      "name": "CTW Pants",
-      "level": 5,
-      "materials": {"weirwood": 10240, "leather-straps": 10240, "black-iron": 10240},
-      "img": "item/ctw-pants.png",
-	  "warlord": true
-    },
-	{
-      "name": "CTW Boots",
-      "level": 5,
-      "materials": {"milk-of-the-poppy": 10240, "hide": 10240, "ironwood": 10240},
-      "img": "item/ctw-boots.png",
-	  "warlord": true
-    },
-	{
-      "name": "CTW Ring",
-      "level": 5,
-      "materials": {"ironwood": 10240, "goldenheart-wood": 10240, "wildfire": 10240},
-      "img": "item/ctw-ring.png",
-	  "warlord": true
-    },
-	{
-      "name": "CTW Sword",
-      "level": 5,
-      "materials": {"copper-bar": 10240, "dragonglass": 10240, "leather-straps": 10240},
-      "img": "item/ctw-weapon.png",
-	  "warlord": true
-    },
-    {
-      "name": "Copper Pot",
-      "level": 10,
-      "materials": {"copper-bar": 30720, "leather-straps": 30720},
-      "img": "item/icon_eq_standard_helmet_copperpot.webp"
-    },
-    {
-      "name": "Straw hat",
-      "level": 10,
-      "materials": {"dragonglass": 20480, "silk": 20480, "leather-straps": 20480},
-      "img": "item/icon_eq_standard_helmet_strawhat.webp"
-    },
-    {
-      "name": "Wool Robe",
-      "level": 10,
-      "materials": {"milk-of-the-poppy": 30720, "hide": 30720},
-      "img": "item/icon_eq_standard_chest_woolrobe.webp"
-    },
-    {
-      "name": "Padded Armor",
-      "level": 10,
-      "materials": {"silk": 30720, "ironwood": 30720},
-      "img": "item/icon_eq_standard_chest_paddedarmor.webp"
-    },
-    {
-      "name": "Wool Leggings",
-      "level": 10,
-      "materials": {"hide": 30720, "wildfire": 30720},
-      "img": "item/icon_eq_standard_pants_woolleggings.webp"
-    },
-    {
-      "name": "Ragged Shorts",
-      "level": 10,
-      "materials": {"leather-straps": 30720, "milk-of-the-poppy": 30720},
-      "img": "item/icon_eq_standard_pants_raggedshorts.webp"
-    },
-    {
-      "name": "Velvet Boots",
-      "level": 10,
-      "materials": {"silk": 30720, "wildfire": 30720},
-      "img": "item/icon_eq_standard_boots_velvetboots.webp"
-    },
-    {
-      "name": "Galoshes",
-      "level": 10,
-      "materials": {"weirwood": 30720, "ironwood": 30720},
-      "img": "item/icon_eq_standard_boots_galoshes.webp"
-    },
-    {
-      "name": "Pyrite Lament",
-      "level": 10,
-      "materials": {"dragonglass": 30720, "kingswood-oak": 30720},
-      "img": "item/icon_eq_standard_ring_pyritelament.webp"
-    },
-    {
-      "name": "Quartz Ring",
-      "level": 10,
-      "materials": {"dragonglass": 30720, "weirwood": 30720},
-      "img": "item/icon_eq_standard_ring_quartzring.webp"
-    },
-    {
-      "name": "Mace",
-      "level": 10,
-      "materials": {"weirwood": 30720, "black-iron": 30720},
-      "img": "item/icon_eq_standard_weapon_mace.webp"
-    },
-    {
-      "name": "Short Bow",
-      "level": 10,
-      "materials": {"silk": 30720, "goldenheart-wood": 30720},
-      "img": "item/icon_eq_standard_weapon_shortbow.webp"
-    },
-	{
-      "name": "CTW Helm",
-      "level": 10,
-      "materials": {"kingswood-oak": 20480, "wildfire": 20480, "hide": 20480},
-      "img": "item/ctw-head.png",
-	  "warlord": true
-    },
-	{
-      "name": "CTW Chestplate",
-      "level": 10,
-      "materials": {"silk": 20480, "black-iron": 20480, "copper-bar": 20480},
-      "img": "item/ctw-chest.png",
-	  "warlord": true
-    },
-	{
-      "name": "CTW Pants",
-      "level": 10,
-      "materials": {"weirwood": 20480, "leather-straps": 20480, "black-iron": 20480},
-      "img": "item/ctw-pants.png",
-	  "warlord": true
-    },
-	{
-      "name": "CTW Boots",
-      "level": 10,
-      "materials": {"milk-of-the-poppy": 20480, "hide": 20480, "ironwood": 20480},
-      "img": "item/ctw-boots.png",
-	  "warlord": true
-    },
-	{
-      "name": "CTW Ring",
-      "level": 10,
-      "materials": {"ironwood": 20480, "goldenheart-wood": 20480, "wildfire": 20480},
-      "img": "item/ctw-ring.png",
-	  "warlord": true
-    },	
-	{
-      "name": "CTW Sword",
-      "level": 10,
-      "materials": {"copper-bar": 20480, "dragonglass": 20480, "leather-straps": 20480},
-      "img": "item/ctw-weapon.png",
-	  "warlord": true
-    }
-  ]
+const season0 = {
+	season: 0,
+	sets: [
+		{
+			products: [
+				{
+					"name": "Wooden Bucket",
+					"level": 1,
+					"materials": {"weirwood": 10, "kingswood-oak": 10},
+					"img": "item/icon_eq_standard_helmet_woodenbucket.webp"
+				},
+				{
+					"name": "Leather Vest",
+					"level": 1,
+					"materials": {"hide": 10, "leather-straps": 10},
+					"img": "item/icon_eq_standard_chest_leathervest.webp"
+				},
+				{
+					"name": "Flax Legwraps",
+					"level": 1,
+					"materials": {"milk-of-the-poppy": 10, "goldenheart-wood": 10},
+					"img": "item/icon_eq_standard_pants_flaxlegwraps.webp"
+				},
+				{
+					"name": "Straw Sandals",
+					"level": 1,
+					"materials": {"hide": 10, "leather-straps": 10},
+					"img": "item/icon_eq_standard_boots_strawsandals.webp"
+				},
+				{
+					"name": "Copper Band",
+					"level": 1,
+					"materials": {"black-iron": 10, "copper-bar": 10},
+					"img": "item/icon_eq_standard_ring_copperband.webp"
+				},
+				{
+					"name": "Dagger",
+					"level": 1,
+					"materials": {"ironwood": 10, "kingswood-oak": 10},
+					"img": "item/icon_eq_standard_weapon_shortsword.webp"
+				},
+				{
+					"name": "Warlord helmet",
+					"level": 1,
+					"materials": {"silk": 6, "black-iron": 6, "copper-bar": 6},
+					"img": "item/ctw-head.png",
+					"warlord": true
+				},
+				{
+					"name": "Warlord plate",
+					"level": 1,
+					"materials": {"milk-of-the-poppy": 6, "hide": 6, "ironwood": 6},
+					"img": "item/ctw-chest.png",
+					"warlord": true
+				},
+				{
+					"name": "Warlord pants",
+					"level": 1,
+					"materials": {"kingswood-oak": 6, "wildfire": 6, "hide": 6},
+					"img": "item/ctw-pants.png",
+					"warlord": true
+				},
+				{
+					"name": "Warlord dagger",
+					"level": 1,
+					"materials": {"ironwood": 6, "goldenheart-wood": 6, "wildfire": 6},
+					"img": "item/ctw-weapon.png",
+					"warlord": true
+				},
+				{
+					"name": "Warlord ring",
+					"level": 1,
+					"materials": {"weirwood": 6, "leather-straps": 6, "black-iron": 6},
+					"img": "item/ctw-ring.png",
+					"warlord": true
+				},
+				{
+					"name": "Warlord shoos",
+					"level": 1,
+					"materials": {"ironwood": 6, "goldenheart-wood": 6, "wildfire": 6},
+					"img": "item/ctw-boots.png",
+					"warlord": true
+				},
+				{
+					"name": "Wool Bandana",
+					"level": 5,
+					"materials": {"kingswood-oak": 15, "milk-of-the-poppy": 15},
+					"img": "item/icon_eq_standard_helmet_woolbandana.webp"
+				},
+				{
+					"name": "Boiled Leather Shirt",
+					"level": 5,
+					"materials": {"hide": 15, "dragonglass": 15},
+					"img": "item/icon_eq_standard_chest_boiledleathershirt.webp"
+				},
+				{
+					"name": "Leather Poleyns",
+					"level": 5,
+					"materials": {"hide": 15, "leather-straps": 15},
+					"img": "item/icon_eq_standard_pants_leatherpoleyns.webp"
+				},
+				{
+					"name": "Leather Booties",
+					"level": 5,
+					"materials": {"hide": 10, "silk": 10, "weirwood": 10},
+					"img": "item/icon_eq_standard_boots_leatherbooties.webp"
+				},
+				{
+					"name": "Bronze Band",
+					"level": 5,
+					"materials": {"copper-bar": 15, "milk-of-the-poppy": 15},
+					"img": "item/icon_eq_standard_ring_bronzeband.webp"
+				},
+				{
+					"name": "Spear",
+					"level": 5,
+					"materials": {"leather-straps": 15, "wildfire": 15},
+					"img": "item/icon_eq_standard_weapon_spear.webp"
+				},	
+				{
+					"name": "Warlord helmet",
+					"level": 5,
+					"materials": {"kingswood-oak": 10, "wildfire": 10, "hide": 10},
+					"img": "item/ctw-head.png",
+					"warlord": true
+				},
+				{
+					"name": "Warlord plate",
+					"level": 5,
+					"materials": {"silk": 10, "black-iron": 10, "copper-bar": 10},
+					"img": "item/ctw-chest.png",
+					"warlord": true
+				},
+				{
+					"name": "Warlord pants",
+					"level": 5,
+					"materials": {"weirwood": 10, "leather-straps": 10, "black-iron": 10},
+					"img": "item/ctw-pants.png",
+					"warlord": true
+				},
+				{
+					"name": "Warlord dagger",
+					"level": 5,
+					"materials": {"copper-bar": 10, "dragonglass": 10, "leather-straps": 10},
+					"img": "item/ctw-weapon.png",
+					"warlord": true
+				},
+				{
+					"name": "Warlord ring",
+					"level": 5,
+					"materials": {"ironwood": 10, "goldenheart-wood": 10, "wildfire": 10},
+					"img": "item/ctw-ring.png",
+					"warlord": true
+				},
+				{
+					"name": "Warlord shoos",
+					"level": 5,
+					"materials": {"milk-of-the-poppy": 10, "hide": 10, "ironwood": 10},
+					"img": "item/ctw-boots.png",
+					"warlord": true
+				},
+				{
+					"name": "Warlord helmet",
+					"level": 10,
+					"materials": {"kingswood-oak": 20, "wildfire": 20, "hide": 20},
+					"img": "item/ctw-head.png",
+					"warlord": true
+				},
+				{
+					"name": "Warlord plate",
+					"level": 10,
+					"materials": {"silk": 20, "black-iron": 20, "copper-bar": 20},
+					"img": "item/ctw-chest.png",
+					"warlord": true
+				},
+				{
+					"name": "Warlord pants",
+					"level": 10,
+					"materials": {"weirwood": 20, "leather-straps": 20, "black-iron": 20},
+					"img": "item/ctw-pants.png",
+					"warlord": true
+				},
+				{
+					"name": "Warlord dagger",
+					"level": 10,
+					"materials": {"copper-bar": 20, "dragonglass": 20, "leather-straps": 20},
+					"img": "item/ctw-weapon.png",
+					"warlord": true
+				},
+				{
+					"name": "Warlord ring",
+					"level": 10,
+					"materials": {"ironwood": 20, "goldenheart-wood": 20, "wildfire": 20},
+					"img": "item/ctw-ring.png",
+					"warlord": true
+				},
+				{
+					"name": "Warlord shoos",
+					"level": 10,
+					"materials": {"milk-of-the-poppy": 20, "hide": 20, "ironwood": 20},
+					"img": "item/ctw-boots.png",
+					"warlord": true
+				},	
+				{
+					"name": "Copper Pot",
+					"level": 10,
+					"materials": {"copper-bar": 30, "leather-straps": 30},
+					"img": "item/icon_eq_standard_helmet_copperpot.webp"
+				},
+				{
+					"name": "Straw hat",
+					"level": 10,
+					"materials": {"dragonglass": 20, "silk": 20, "leather-straps": 20},
+					"img": "item/icon_eq_standard_helmet_strawhat.webp"
+				},
+				{
+					"name": "Wool Robe",
+					"level": 10,
+					"materials": {"milk-of-the-poppy": 30, "hide": 30},
+					"img": "item/icon_eq_standard_chest_woolrobe.webp"
+				},
+				{
+					"name": "Padded Armor",
+					"level": 10,
+					"materials": {"silk": 30, "ironwood": 30},
+					"img": "item/icon_eq_standard_chest_paddedarmor.webp"
+				},
+				{
+					"name": "Wool Leggings",
+					"level": 10,
+					"materials": {"hide": 30, "wildfire": 30},
+					"img": "item/icon_eq_standard_pants_woolleggings.webp"
+				},
+				{
+					"name": "Ragged Shorts",
+					"level": 10,
+					"materials": {"leather-straps": 30, "milk-of-the-poppy": 30},
+					"img": "item/icon_eq_standard_pants_raggedshorts.webp"
+				},
+				{
+					"name": "Velvet Boots",
+					"level": 10,
+					"materials": {"silk": 30, "wildfire": 30},
+					"img": "item/icon_eq_standard_boots_velvetboots.webp"
+				},
+				{
+					"name": "Galoshes",
+					"level": 10,
+					"materials": {"weirwood": 30, "ironwood": 30},
+					"img": "item/icon_eq_standard_boots_galoshes.webp"
+				},
+				{
+					"name": "Pyrite Lament",
+					"level": 10,
+					"materials": {"dragonglass": 30, "kingswood-oak": 30},
+					"img": "item/icon_eq_standard_ring_pyritelament.webp"
+				},
+				{
+					"name": "Quartz Ring",
+					"level": 10,
+					"materials": {"dragonglass": 30, "weirwood": 30},
+					"img": "item/icon_eq_standard_ring_quartzring.webp"
+				},
+				{
+					"name": "Mace",
+					"level": 10,
+					"materials": {"weirwood": 30, "black-iron": 30},
+					"img": "item/icon_eq_standard_weapon_mace.webp"
+				},
+				{
+					"name": "Short Bow",
+					"level": 10,
+					"materials": {"silk": 30, "goldenheart-wood": 30},
+					"img": "item/icon_eq_standard_weapon_shortbow.webp"
+				}
+			]
+		}
+	]
+}
+const season10 = {
+	season: 10,
+	sets: [
+		{
+			setName: "Dragon Heiress's",
+			setMat: "Sanguine Thread",
+			products: [
+				{
+					name: "Wrought Iron Obelisk",
+					level: 5,
+					materials: {
+					"sanguine-thread": 10,
+					"milk-of-the-poppy": 10,
+					"copper-bar": 10
+				},
+					img: "item/idragon_heiress/head.png",
+				},
+				{
+					name: "Sanguine Mantle",
+					level: 5,
+					materials: {
+						"sanguine-thread": 10,
+						"leather-straps": 10,
+						"goldenheart-wood": 10
+					},
+					img: "item/idragon_heiress/chest.png",
+				},
+				{
+					name: "Sanguine Skirt",
+					level: 5,
+					materials: {
+						"sanguine-thread": 10,
+						"copper-bar": 10,
+						"silk": 10
+					},
+					img: "item/dragon_heiress/pants.png",
+				},
+				{
+					name: "Gleaming Slippers",
+					level: 5,
+					materials: {
+						"sanguine-thread": 10,
+						"silk": 10,
+						"kingswood-oak": 10
+					},
+					img: "item/dragon_heiress/boots.png",
+				},
+				{
+					name: "Trifecta Ring",
+					level: 5,
+					materials: {
+						"sanguine-thread": 10,
+						"hide": 10,
+						"ironwood": 10
+					},
+					img: "item/dragon_heiress/ring.png",
+				},
+				{
+					name: "Wrought Iron Obelisk",
+					level: 5,
+					materials: {
+						"sanguine-thread": 10,
+						"milk-of-the-poppy": 10,
+						"copper-bar": 10
+					},
+					img: "item/dragon_heiress/weapon.png",
+				}
+			]
+		},
+		{
+		setName: "Fishmonger",
+		setMat: "Fishmongers Fishing Net",
+		products: [
+			{
+				name: "Gilded Hairpin",
+				level: 5,
+				materials: {
+					"fishmongers-fishing-net": 10,
+					"hide": 10,
+					"dragonglass": 10
+				},
+					img: "item/fishmonger/head.png",
+				},
+			]
+		}
+	]
 };
+
+
+let craftItem = {
+	products: [
+		...season0.sets.flatMap(set =>
+			set.products.map(product => ({
+				...product,
+				setName: set.setName,
+				season: season0.season
+			}))
+		),
+		...season10.sets.flatMap(set =>
+			set.products.map(product => ({
+				...product,
+				setName: set.setName,
+				season: season10.season
+			}))
+		)
+	]
+};
+
+
+
