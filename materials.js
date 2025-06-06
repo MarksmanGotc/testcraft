01eae73ccf4cab7962654b9d6a4dc998d3aa4167
MarksmanGotc <151929315+MarksmanGotc@users.noreply.github.com>
2025-06-06 15:19:13 +0300

    Update materials.js

--- materials.js
+++ materials.js
@@ -1,50 +1,68 @@
 const materials = {
-	"black-iron": {
-        "img": "materials/icon_crafting_blackiron.webp",
-		"Original-name": "Black Iron"
-    },
-	"copper-bar": {
-        "img": "materials/icon_crafting_copper_bar.webp",
-		"Original-name": "Copper Bar"
-    },
-    "dragonglass": {
-        "img": "materials/icon_crafting_dragonglass.webp",
-		"Original-name": "Dragonglass"
-    },
-    "goldenheart-wood": {
-        "img": "materials/icon_crafting_goldenheart_wood.webp",
-		"Original-name": "Goldenheart Wood"
-    },
-    "hide": {
-        "img": "materials/icon_crafting_hide.webp",
-		"Original-name": "Hide"
-    },
-    "ironwood": {
-        "img": "materials/icon_crafting_ironwood.webp",
-		"Original-name": "Ironwood"
-    },
-    "kingswood-oak": {
-        "img": "materials/icon_crafting_kingswood_oak.webp",
-		"Original-name": "Kingswood Oak"
-    },
-    "leather-straps": {
-        "img": "materials/icon_crafting_leather_straps.webp",
-		"Original-name": "Leather Straps"
-    },
-    "milk-of-the-poppy": {
-        "img": "materials/icon_crafting_milk_of_the_poppy.webp",
-		"Original-name": "Milk of the poppy"
-    },
-    "silk": {
-        "img": "materials/icon_crafting_silk.webp",
-		"Original-name": "Silk"
-    },
-    "weirwood": {
-        "img": "materials/icon_crafting_weir_wood.webp",
-		"Original-name": "Weirwood"
-    },
-    "wildfire": {
-        "img": "materials/icon_crafting_wild_fire.webp",
-		"Original-name": "Wildfire"
-    }
-};
+	0: {
+		season: 0,
+		mats: {
+			"black-iron": {
+				"img": "materials/icon_crafting_blackiron.webp",
+				"Orginal-name": "Black Iron"
+			},
+			"copper-bar": {
+				"img": "materials/icon_crafting_copper_bar.webp",
+				"Orginal-name": "Copper Bar"
+			},
+			"dragonglass": {
+				"img": "materials/icon_crafting_dragonglass.webp",
+				"Orginal-name": "Dragonglass"
+			},
+			"goldenheart-wood": {
+				"img": "materials/icon_crafting_goldenheart_wood.webp",
+				"Orginal-name": "Goldenheart Wood"
+			},
+			"hide": {
+				"img": "materials/icon_crafting_hide.webp",
+				"Orginal-name": "Hide"
+			},
+			"ironwood": {
+				"img": "materials/icon_crafting_ironwood.webp",
+				"Orginal-name": "Ironwood"
+			},
+			"kingswood-oak": {
+				"img": "materials/icon_crafting_kingswood_oak.webp",
+				"Orginal-name": "Kingswood Oak"
+			},
+			"leather-straps": {
+				"img": "materials/icon_crafting_leather_straps.webp",
+				"Orginal-name": "Leather Straps"
+			},
+			"milk-of-the-poppy": {
+				"img": "materials/icon_crafting_milk_of_the_poppy.webp",
+				"Orginal-name": "Milk of the poppy"
+			},
+			"silk": {
+				"img": "materials/icon_crafting_silk.webp",
+				"Orginal-name": "Silk"
+			},
+			"weirwood": {
+				"img": "materials/icon_crafting_weir_wood.webp",
+				"Orginal-name": "Weirwood"
+			},
+			"wildfire": {
+				"img": "materials/icon_crafting_wild_fire.webp",
+				"Orginal-name": "Wildfire"
+			}
+		}
+	},
+	10: {
+		season: 10,
+		mats: {
+			"sanguine-thread": {
+				"img": "materials/season10/sanguine-thread.png",
+				"Orginal-name": "Sanguine Thread"
+			},
+			"fishmongers-fishing-net": {
+				"img": "materials/season10/fishmongers-fishing-net.png",
+				"Orginal-name": "Fishmonger's Fishing Net"
+			},
+		}
+	}
+}
