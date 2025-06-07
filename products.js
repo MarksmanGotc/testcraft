37647f4142a5fcbdca4e6e9c556f7e1740e0357d
MarksmanGotc <151929315+MarksmanGotc@users.noreply.github.com>
2025-06-07 14:56:36 +0300

    Update products.js

--- products.js
+++ products.js
@@ -61,10 +61,10 @@ const season0 = {
 					"warlord": true
 				},
 				{
-					"name": "Warlord dagger",
+					"name": "Warlord shoos",
 					"level": 1,
 					"materials": {"ironwood": 6, "goldenheart-wood": 6, "wildfire": 6},
-					"img": "item/ctw-weapon.png",
+					"img": "item/ctw-boots.png",
 					"warlord": true
 				},
 				{
@@ -75,10 +75,10 @@ const season0 = {
 					"warlord": true
 				},
 				{
-					"name": "Warlord shoos",
+					"name": "Warlord dagger",
 					"level": 1,
 					"materials": {"ironwood": 6, "goldenheart-wood": 6, "wildfire": 6},
-					"img": "item/ctw-boots.png",
+					"img": "item/ctw-weapon.png",
 					"warlord": true
 				},
 				{
@@ -139,10 +139,10 @@ const season0 = {
 					"warlord": true
 				},
 				{
-					"name": "Warlord dagger",
+					"name": "Warlord shoos",
 					"level": 5,
-					"materials": {"copper-bar": 10, "dragonglass": 10, "leather-straps": 10},
-					"img": "item/ctw-weapon.png",
+					"materials": {"milk-of-the-poppy": 10, "hide": 10, "ironwood": 10},
+					"img": "item/ctw-boots.png",
 					"warlord": true
 				},
 				{
@@ -153,10 +153,10 @@ const season0 = {
 					"warlord": true
 				},
 				{
-					"name": "Warlord shoos",
+					"name": "Warlord dagger",
 					"level": 5,
-					"materials": {"milk-of-the-poppy": 10, "hide": 10, "ironwood": 10},
-					"img": "item/ctw-boots.png",
+					"materials": {"copper-bar": 10, "dragonglass": 10, "leather-straps": 10},
+					"img": "item/ctw-weapon.png",
 					"warlord": true
 				},
 				{
@@ -181,10 +181,10 @@ const season0 = {
 					"warlord": true
 				},
 				{
-					"name": "Warlord dagger",
+					"name": "Warlord shoos",
 					"level": 10,
-					"materials": {"copper-bar": 20, "dragonglass": 20, "leather-straps": 20},
-					"img": "item/ctw-weapon.png",
+					"materials": {"milk-of-the-poppy": 20, "hide": 20, "ironwood": 20},
+					"img": "item/ctw-boots.png",
 					"warlord": true
 				},
 				{
@@ -195,12 +195,12 @@ const season0 = {
 					"warlord": true
 				},
 				{
-					"name": "Warlord shoos",
+					"name": "Warlord dagger",
 					"level": 10,
-					"materials": {"milk-of-the-poppy": 20, "hide": 20, "ironwood": 20},
-					"img": "item/ctw-boots.png",
+					"materials": {"copper-bar": 20, "dragonglass": 20, "leather-straps": 20},
+					"img": "item/ctw-weapon.png",
 					"warlord": true
-				},	
+				},
 				{
 					"name": "Copper Pot",
 					"level": 10,
@@ -398,7 +398,49 @@ const season0 = {
 					"materials": {"ironwood": 180, "copper-bar": 180},
 					"odds": "low",
 					"img": "item/15-25/icon_eq_standard_weapon_shortsword.png"
-				}
+				},
+				{
+					"name": "Warlord helmet",
+					"level": 15,
+					"materials": {"kingswood-oak": 120, "wildfire": 120, "hide": 120},
+					"img": "item/ctw-head.png",
+					"warlord": true
+				},
+				{
+					"name": "Warlord plate",
+					"level": 15,
+					"materials": {"silk": 120, "black-iron": 120, "copper-bar": 120},
+					"img": "item/ctw-chest.png",
+					"warlord": true
+				},
+				{
+					"name": "Warlord pants",
+					"level": 15,
+					"materials": {"weirwood": 120, "leather-straps": 120, "black-iron": 120},
+					"img": "item/ctw-pants.png",
+					"warlord": true
+				},
+				{
+					"name": "Warlord shoos",
+					"level": 15,
+					"materials": {"milk-of-the-poppy": 120, "hide": 120, "ironwood": 120},
+					"img": "item/ctw-boots.png",
+					"warlord": true
+				},
+				{
+					"name": "Warlord ring",
+					"level": 15,
+					"materials": {"ironwood": 120, "goldenheart-wood": 120, "wildfire": 120},
+					"img": "item/ctw-ring.png",
+					"warlord": true
+				},
+				{
+					"name": "Warlord dagger",
+					"level": 15,
+					"materials": {"copper-bar": 120, "dragonglass": 120, "leather-straps": 120},
+					"img": "item/ctw-weapon.png",
+					"warlord": true
+				},
 			]
 		}
 	]
@@ -469,23 +511,577 @@ const season10 = {
 						"copper-bar": 10
 					},
 					img: "item/dragon_heiress/weapon.png",
+				},
+				{
+					name: "Wrought Iron Obelisk",
+					level: 10,
+					materials: {
+					"sanguine-thread": 20,
+					"milk-of-the-poppy": 20,
+					"copper-bar": 20
+				},
+					img: "item/idragon_heiress/head.png",
+				},
+				{
+					name: "Sanguine Mantle",
+					level: 10,
+					materials: {
+						"sanguine-thread": 20,
+						"leather-straps": 20,
+						"goldenheart-wood": 20
+					},
+					img: "item/idragon_heiress/chest.png",
+				},
+				{
+					name: "Sanguine Skirt",
+					level: 10,
+					materials: {
+						"sanguine-thread": 20,
+						"copper-bar": 20,
+						"silk": 20
+					},
+					img: "item/dragon_heiress/pants.png",
+				},
+				{
+					name: "Gleaming Slippers",
+					level: 10,
+					materials: {
+						"sanguine-thread": 20,
+						"silk": 20,
+						"kingswood-oak": 20
+					},
+					img: "item/dragon_heiress/boots.png",
+				},
+				{
+					name: "Trifecta Ring",
+					level: 10,
+					materials: {
+						"sanguine-thread": 20,
+						"hide": 20,
+						"ironwood": 20
+					},
+					img: "item/dragon_heiress/ring.png",
+				},
+				{
+					name: "Wrought Iron Obelisk",
+					level: 10,
+					materials: {
+						"sanguine-thread": 20,
+						"milk-of-the-poppy": 20,
+						"copper-bar": 20
+					},
+					img: "item/dragon_heiress/weapon.png",
+				},
+				{
+					name: "Wrought Iron Obelisk",
+					level: 15,
+					materials: {
+					"sanguine-thread": 120,
+					"milk-of-the-poppy": 120,
+					"copper-bar": 120
+				},
+					img: "item/idragon_heiress/head.png",
+				},
+				{
+					name: "Sanguine Mantle",
+					level: 15,
+					materials: {
+						"sanguine-thread": 120,
+						"leather-straps": 120,
+						"goldenheart-wood": 120
+					},
+					img: "item/idragon_heiress/chest.png",
+				},
+				{
+					name: "Sanguine Skirt",
+					level: 15,
+					materials: {
+						"sanguine-thread": 120,
+						"copper-bar": 120,
+						"silk": 120
+					},
+					img: "item/dragon_heiress/pants.png",
+				},
+				{
+					name: "Gleaming Slippers",
+					level: 15,
+					materials: {
+						"sanguine-thread": 120,
+						"silk": 120,
+						"kingswood-oak": 120
+					},
+					img: "item/dragon_heiress/boots.png",
+				},
+				{
+					name: "Trifecta Ring",
+					level: 15,
+					materials: {
+						"sanguine-thread": 120,
+						"hide": 120,
+						"ironwood": 120
+					},
+					img: "item/dragon_heiress/ring.png",
+				},
+				{
+					name: "Wrought Iron Obelisk",
+					level: 15,
+					materials: {
+						"sanguine-thread": 120,
+						"milk-of-the-poppy": 120,
+						"copper-bar": 120
+					},
+					img: "item/dragon_heiress/weapon.png",
+				},
+				{
+					name: "Wrought Iron Obelisk",
+					level: 20,
+					materials: {
+					"sanguine-thread": 400,
+					"milk-of-the-poppy": 400,
+					"copper-bar": 400,
+					"ironwood": 400
+				},
+					img: "item/idragon_heiress/head.png",
+				},
+				{
+					name: "Sanguine Mantle",
+					level: 20,
+					materials: {
+						"sanguine-thread": 400,
+						"leather-straps": 400,
+						"goldenheart-wood": 400,
+						"black-iron": 400
+					},
+					img: "item/idragon_heiress/chest.png",
+				},
+				{
+					name: "Sanguine Skirt",
+					level: 20,
+					materials: {
+						"sanguine-thread": 400,
+						"copper-bar": 400,
+						"silk": 400,
+						"kingswood-oak": 400
+					},
+					img: "item/dragon_heiress/pants.png",
+				},
+				{
+					name: "Gleaming Slippers",
+					level: 20,
+					materials: {
+						"sanguine-thread": 400,
+						"silk": 400,
+						"kingswood-oak": 400,
+						"weirwood": 400
+					},
+					img: "item/dragon_heiress/boots.png",
+				},
+				{
+					name: "Trifecta Ring",
+					level: 20,
+					materials: {
+						"sanguine-thread": 400,
+						"hide": 400,
+						"ironwood": 400,
+						"milk-of-the-poppy": 400
+					},
+					img: "item/dragon_heiress/ring.png",
+				},
+				{
+					name: "Wrought Iron Obelisk",
+					level: 20,
+					materials: {
+						"sanguine-thread": 400,
+						"milk-of-the-poppy": 400,
+						"copper-bar": 400,
+						"goldenheart-wood": 400
+					},
+					img: "item/dragon_heiress/weapon.png",
+				},
+				{
+					name: "Wrought Iron Obelisk",
+					level: 25,
+					materials: {
+					"sanguine-thread": 1200,
+					"milk-of-the-poppy": 1200,
+					"copper-bar": 1200,
+					"ironwood": 1200
+				},
+					img: "item/idragon_heiress/head.png",
+				},
+				{
+					name: "Sanguine Mantle",
+					level: 25,
+					materials: {
+						"sanguine-thread": 1200,
+						"leather-straps": 1200,
+						"goldenheart-wood": 1200,
+						"black-iron": 1200
+					},
+					img: "item/idragon_heiress/chest.png",
+				},
+				{
+					name: "Sanguine Skirt",
+					level: 25,
+					materials: {
+						"sanguine-thread": 1200,
+						"copper-bar": 1200,
+						"silk": 1200,
+						"kingswood-oak": 1200
+					},
+					img: "item/dragon_heiress/pants.png",
+				},
+				{
+					name: "Gleaming Slippers",
+					level: 25,
+					materials: {
+						"sanguine-thread": 1200,
+						"silk": 1200,
+						"kingswood-oak": 1200,
+						"weirwood": 1200
+					},
+					img: "item/dragon_heiress/boots.png",
+				},
+				{
+					name: "Trifecta Ring",
+					level: 25,
+					materials: {
+						"sanguine-thread": 1200,
+						"hide": 1200,
+						"ironwood": 1200,
+						"milk-of-the-poppy": 1200
+					},
+					img: "item/dragon_heiress/ring.png",
+				},
+				{
+					name: "Wrought Iron Obelisk",
+					level: 25,
+					materials: {
+						"sanguine-thread": 1200,
+						"milk-of-the-poppy": 1200,
+						"copper-bar": 1200,
+						"goldenheart-wood": 1200
+					},
+					img: "item/dragon_heiress/weapon.png",
 				}
 			]
 		},
 		{
-		setName: "Fishmonger",
-		setMat: "Fishmongers Fishing Net",
-		products: [
-			{
-				name: "Gilded Hairpin",
-				level: 5,
-				materials: {
-					"fishmongers-fishing-net": 10,
-					"hide": 10,
-					"dragonglass": 10
+			setName: "Fishmonger",
+			setMat: "Fishmongers Fishing Net",
+			products: [
+				{
+					name: "Gilded Hairpin",
+					level: 5,
+					materials: {
+						"fishmongers-fishing-net": 10,
+						"hide": 10,
+						"dragonglass": 10
+					},
+						img: "item/fishmonger/head.png",
 				},
-					img: "item/fishmonger/head.png",
+				{
+					name: "Tassled Jerkin",
+					level: 5,
+					materials: {
+						"fishmongers-fishing-net": 10,
+						"silk": 10,
+						"goldenheart-wood": 10
+					},
+						img: "item/fishmonger/chest.png",
 				},
+				{
+					name: "Tassled Skirt",
+					level: 5,
+					materials: {
+						"fishmongers-fishing-net": 10,
+						"weirwood": 10,
+						"silk": 10
+					},
+						img: "item/fishmonger/pants.png",
+				},
+				{
+					name: "Dry Boots",
+					level: 5,
+					materials: {
+						"fishmongers-fishing-net": 10,
+						"leather-straps": 10,
+						"kingswood-oak": 10
+					},
+						img: "item/fishmonger/boots.png",
+				},
+				{
+					name: "Macrame Ring",
+					level: 5,
+					materials: {
+						"fishmongers-fishing-net": 10,
+						"copper-bar": 10,
+						"ironwood": 10
+					},
+						img: "item/fishmonger/ring.png",
+				},
+				{
+					name: "Gutting Knife",
+					level: 5,
+					materials: {
+						"fishmongers-fishing-net": 10,
+						"milk-of-the-poppy": 10,
+						"copper-bar": 10
+					},
+						img: "item/fishmonger/weapon.png",
+				},
+				{
+					name: "Gilded Hairpin",
+					level: 10,
+					materials: {
+						"fishmongers-fishing-net": 20,
+						"hide": 20,
+						"dragonglass": 20
+					},
+						img: "item/fishmonger/head.png",
+				},
+				{
+					name: "Tassled Jerkin",
+					level: 10,
+					materials: {
+						"fishmongers-fishing-net": 20,
+						"silk": 20,
+						"goldenheart-wood": 20
+					},
+						img: "item/fishmonger/chest.png",
+				},
+				{
+					name: "Tassled Skirt",
+					level: 10,
+					materials: {
+						"fishmongers-fishing-net": 20,
+						"weirwood": 20,
+						"silk": 20
+					},
+						img: "item/fishmonger/pants.png",
+				},
+				{
+					name: "Dry Boots",
+					level: 10,
+					materials: {
+						"fishmongers-fishing-net": 20,
+						"leather-straps": 20,
+						"kingswood-oak": 20
+					},
+						img: "item/fishmonger/boots.png",
+				},
+				{
+					name: "Macrame Ring",
+					level: 10,
+					materials: {
+						"fishmongers-fishing-net": 20,
+						"copper-bar": 20,
+						"ironwood": 20
+					},
+						img: "item/fishmonger/ring.png",
+				},
+				{
+					name: "Gutting Knife",
+					level: 10,
+					materials: {
+						"fishmongers-fishing-net": 20,
+						"milk-of-the-poppy": 20,
+						"copper-bar": 20
+					},
+						img: "item/fishmonger/weapon.png",
+				},
+				{
+					name: "Gilded Hairpin",
+					level: 15,
+					materials: {
+						"fishmongers-fishing-net": 120,
+						"hide": 120,
+						"dragonglass": 120
+					},
+						img: "item/fishmonger/head.png",
+				},
+				{
+					name: "Tassled Jerkin",
+					level: 15,
+					materials: {
+						"fishmongers-fishing-net": 120,
+						"silk": 120,
+						"goldenheart-wood": 120
+					},
+						img: "item/fishmonger/chest.png",
+				},
+				{
+					name: "Tassled Skirt",
+					level: 15,
+					materials: {
+						"fishmongers-fishing-net": 120,
+						"weirwood": 120,
+						"silk": 120
+					},
+						img: "item/fishmonger/pants.png",
+				},
+				{
+					name: "Dry Boots",
+					level: 15,
+					materials: {
+						"fishmongers-fishing-net": 120,
+						"leather-straps": 120,
+						"kingswood-oak": 120
+					},
+						img: "item/fishmonger/boots.png",
+				},
+				{
+					name: "Macrame Ring",
+					level: 15,
+					materials: {
+						"fishmongers-fishing-net": 120,
+						"copper-bar": 120,
+						"ironwood": 120
+					},
+						img: "item/fishmonger/ring.png",
+				},
+				{
+					name: "Gutting Knife",
+					level: 15,
+					materials: {
+						"fishmongers-fishing-net": 120,
+						"milk-of-the-poppy": 120,
+						"copper-bar": 120
+					},
+						img: "item/fishmonger/weapon.png",
+				},
+				{
+					name: "Gilded Hairpin",
+					level: 20,
+					materials: {
+						"fishmongers-fishing-net": 400,
+						"hide": 400,
+						"dragonglass": 400,
+						"goldenheart-wood": 400
+					},
+						img: "item/fishmonger/head.png",
+				},
+				{
+					name: "Tassled Jerkin",
+					level: 20,
+					materials: {
+						"fishmongers-fishing-net": 400,
+						"silk": 400,
+						"goldenheart-wood": 400,
+						"black-iron": 400
+					},
+						img: "item/fishmonger/chest.png",
+				},
+				{
+					name: "Tassled Skirt",
+					level: 20,
+					materials: {
+						"fishmongers-fishing-net": 400,
+						"weirwood": 400,
+						"silk": 400,
+						"milk-of-the-poppy": 400
+					},
+						img: "item/fishmonger/pants.png",
+				},
+				{
+					name: "Dry Boots",
+					level: 20,
+					materials: {
+						"fishmongers-fishing-net": 400,
+						"leather-straps": 400,
+						"kingswood-oak": 400,
+						"wildfire": 400
+					},
+						img: "item/fishmonger/boots.png",
+				},
+				{
+					name: "Macrame Ring",
+					level: 20,
+					materials: {
+						"fishmongers-fishing-net": 400,
+						"copper-bar": 400,
+						"ironwood": 400,
+						"kingswood-oak": 400
+					},
+						img: "item/fishmonger/ring.png",
+				},
+				{
+					name: "Gutting Knife",
+					level: 20,
+					materials: {
+						"fishmongers-fishing-net": 400,
+						"milk-of-the-poppy": 400,
+						"copper-bar": 400,
+						"ironwood": 400
+					},
+						img: "item/fishmonger/weapon.png",
+				},
+				{
+					name: "Gilded Hairpin",
+					level: 25,
+					materials: {
+						"fishmongers-fishing-net": 1200,
+						"hide": 1200,
+						"dragonglass": 1200,
+						"goldenheart-wood": 1200
+					},
+						img: "item/fishmonger/head.png",
+				},
+				{
+					name: "Tassled Jerkin",
+					level: 25,
+					materials: {
+						"fishmongers-fishing-net": 1200,
+						"silk": 1200,
+						"goldenheart-wood": 1200,
+						"black-iron": 1200
+					},
+						img: "item/fishmonger/chest.png",
+				},
+				{
+					name: "Tassled Skirt",
+					level: 25,
+					materials: {
+						"fishmongers-fishing-net": 1200,
+						"weirwood": 1200,
+						"silk": 1200,
+						"milk-of-the-poppy": 1200
+					},
+						img: "item/fishmonger/pants.png",
+				},
+				{
+					name: "Dry Boots",
+					level: 25,
+					materials: {
+						"fishmongers-fishing-net": 1200,
+						"leather-straps": 1200,
+						"kingswood-oak": 1200,
+						"wildfire": 1200
+					},
+						img: "item/fishmonger/boots.png",
+				},
+				{
+					name: "Macrame Ring",
+					level: 25,
+					materials: {
+						"fishmongers-fishing-net": 1200,
+						"copper-bar": 1200,
+						"ironwood": 1200,
+						"kingswood-oak": 1200
+					},
+						img: "item/fishmonger/ring.png",
+				},
+				{
+					name: "Gutting Knife",
+					level: 25,
+					materials: {
+						"fishmongers-fishing-net": 1200,
+						"milk-of-the-poppy": 1200,
+						"copper-bar": 1200,
+						"ironwood": 1200
+					},
+						img: "item/fishmonger/weapon.png",
+				}
 			]
 		}
 	]
