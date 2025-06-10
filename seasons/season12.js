e6ba4e82a6ee8a5669a9098c4342c7cc5ccb3250
MarksmanGotc <151929315+MarksmanGotc@users.noreply.github.com>
2025-06-10 21:20:49 +0300

    Add files via upload

---
+++ seasons/season12.js
@@ -0,0 +1,649 @@
+const season12 = {
+	season: 12,
+	sets: [
+		{
+		  "setName": "Burning Usurper's",
+		  "setMat": "Burning Stag Hide",
+		  "products": [
+			{
+			  "name": "Antlered Helm",
+			  "level": 5,
+			  "materials": {
+				"burning-stag-hide": 10,
+				"weirwood": 10,
+				"dragonglass": 10
+			  },
+			  "img": "item/season12/burning-usurper/head.png"
+			},
+			{
+			  "name": "Hauberk",
+			  "level": 5,
+			  "materials": {
+				"burning-stag-hide": 10,
+				"black-iron": 10,
+				"goldenheart-wood": 10
+			  },
+			  "img": "item/season12/burning-usurper/chest.png"
+			},
+			{
+			  "name": "Hide Belt",
+			  "level": 5,
+			  "materials": {
+				"burning-stag-hide": 10,
+				"ironwood": 10,
+				"milk-of-the-poppy": 10
+			  },
+			  "img": "item/season12/burning-usurper/pants.png"
+			},
+			{
+			  "name": "Greaves",
+			  "level": 5,
+			  "materials": {
+				"burning-stag-hide": 10,
+				"leather-straps": 10,
+				"copper-bar": 10
+			  },
+			  "img": "item/season12/burning-usurper/boots.png"
+			},
+			{
+			  "name": "Antlered Ring",
+			  "level": 5,
+			  "materials": {
+				"burning-stag-hide": 10,
+				"hide": 10,
+				"silk": 10
+			  },
+			  "img": "item/season12/burning-usurper/ring.png"
+			},
+			{
+			  "name": "Spiked Warhammer",
+			  "level": 5,
+			  "materials": {
+				"burning-stag-hide": 10,
+				"wildfire": 10,
+				"kingswood-oak": 10
+			  },
+			  "img": "item/season12/burning-usurper/weapon.png"
+			},
+
+			{
+			  "name": "Antlered Helm",
+			  "level": 10,
+			  "materials": {
+				"burning-stag-hide": 20,
+				"weirwood": 20,
+				"dragonglass": 20
+			  },
+			  "img": "item/season12/burning-usurper/head.png"
+			},
+			{
+			  "name": "Hauberk",
+			  "level": 10,
+			  "materials": {
+				"burning-stag-hide": 20,
+				"black-iron": 20,
+				"goldenheart-wood": 20
+			  },
+			  "img": "item/season12/burning-usurper/chest.png"
+			},
+			{
+			  "name": "Hide Belt",
+			  "level": 10,
+			  "materials": {
+				"burning-stag-hide": 20,
+				"ironwood": 20,
+				"milk-of-the-poppy": 20
+			  },
+			  "img": "item/season12/burning-usurper/pants.png"
+			},
+			{
+			  "name": "Greaves",
+			  "level": 10,
+			  "materials": {
+				"burning-stag-hide": 20,
+				"leather-straps": 20,
+				"copper-bar": 20
+			  },
+			  "img": "item/season12/burning-usurper/boots.png"
+			},
+			{
+			  "name": "Antlered Ring",
+			  "level": 10,
+			  "materials": {
+				"burning-stag-hide": 20,
+				"hide": 20,
+				"silk": 20
+			  },
+			  "img": "item/season12/burning-usurper/ring.png"
+			},
+			{
+			  "name": "Spiked Warhammer",
+			  "level": 10,
+			  "materials": {
+				"burning-stag-hide": 20,
+				"wildfire": 20,
+				"kingswood-oak": 20
+			  },
+			  "img": "item/season12/burning-usurper/weapon.png"
+			},
+
+			{
+			  "name": "Antlered Helm",
+			  "level": 15,
+			  "materials": {
+				"burning-stag-hide": 120,
+				"weirwood": 120,
+				"dragonglass": 120
+			  },
+			  "img": "item/season12/burning-usurper/head.png"
+			},
+			{
+			  "name": "Hauberk",
+			  "level": 15,
+			  "materials": {
+				"burning-stag-hide": 120,
+				"black-iron": 120,
+				"goldenheart-wood": 120
+			  },
+			  "img": "item/season12/burning-usurper/chest.png"
+			},
+			{
+			  "name": "Hide Belt",
+			  "level": 15,
+			  "materials": {
+				"burning-stag-hide": 120,
+				"ironwood": 120,
+				"milk-of-the-poppy": 120
+			  },
+			  "img": "item/season12/burning-usurper/pants.png"
+			},
+			{
+			  "name": "Greaves",
+			  "level": 15,
+			  "materials": {
+				"burning-stag-hide": 120,
+				"leather-straps": 120,
+				"copper-bar": 120
+			  },
+			  "img": "item/season12/burning-usurper/boots.png"
+			},
+			{
+			  "name": "Antlered Ring",
+			  "level": 15,
+			  "materials": {
+				"burning-stag-hide": 120,
+				"hide": 120,
+				"silk": 120
+			  },
+			  "img": "item/season12/burning-usurper/ring.png"
+			},
+			{
+			  "name": "Spiked Warhammer",
+			  "level": 15,
+			  "materials": {
+				"burning-stag-hide": 120,
+				"wildfire": 120,
+				"kingswood-oak": 120
+			  },
+			  "img": "item/season12/burning-usurper/weapon.png"
+			},
+
+			{
+			  "name": "Antlered Helm",
+			  "level": 20,
+			  "materials": {
+				"burning-stag-hide": 400,
+				"weirwood": 400,
+				"dragonglass": 400,
+				"hide": 400
+			  },
+			  "img": "item/season12/burning-usurper/head.png"
+			},
+			{
+			  "name": "Hauberk",
+			  "level": 20,
+			  "materials": {
+				"burning-stag-hide": 400,
+				"black-iron": 400,
+				"goldenheart-wood": 400,
+				"wildfire": 400
+			  },
+			  "img": "item/season12/burning-usurper/chest.png"
+			},
+			{
+			  "name": "Hide Belt",
+			  "level": 20,
+			  "materials": {
+				"burning-stag-hide": 400,
+				"ironwood": 400,
+				"milk-of-the-poppy": 400,
+				"weirwood": 400
+			  },
+			  "img": "item/season12/burning-usurper/pants.png"
+			},
+			{
+			  "name": "Greaves",
+			  "level": 20,
+			  "materials": {
+				"burning-stag-hide": 400,
+				"leather-straps": 400,
+				"copper-bar": 400,
+				"ironwood": 400
+			  },
+			  "img": "item/season12/burning-usurper/boots.png"
+			},
+			{
+			  "name": "Antlered Ring",
+			  "level": 20,
+			  "materials": {
+				"burning-stag-hide": 400,
+				"hide": 400,
+				"silk": 400,
+				"black-iron": 400
+			  },
+			  "img": "item/season12/burning-usurper/ring.png"
+			},
+			{
+			  "name": "Spiked Warhammer",
+			  "level": 20,
+			  "materials": {
+				"burning-stag-hide": 400,
+				"wildfire": 400,
+				"kingswood-oak": 400,
+				"leather-straps": 400
+			  },
+			  "img": "item/season12/burning-usurper/weapon.png"
+			},
+
+			{
+			  "name": "Antlered Helm",
+			  "level": 25,
+			  "materials": {
+				"burning-stag-hide": 1200,
+				"weirwood": 1200,
+				"dragonglass": 1200,
+				"hide": 1200
+			  },
+			  "img": "item/season12/burning-usurper/head.png"
+			},
+			{
+			  "name": "Hauberk",
+			  "level": 25,
+			  "materials": {
+				"burning-stag-hide": 1200,
+				"black-iron": 1200,
+				"goldenheart-wood": 1200,
+				"wildfire": 1200
+			  },
+			  "img": "item/season12/burning-usurper/chest.png"
+			},
+			{
+			  "name": "Hide Belt",
+			  "level": 25,
+			  "materials": {
+				"burning-stag-hide": 1200,
+				"ironwood": 1200,
+				"milk-of-the-poppy": 1200,
+				"weirwood": 1200
+			  },
+			  "img": "item/season12/burning-usurper/pants.png"
+			},
+			{
+			  "name": "Greaves",
+			  "level": 25,
+			  "materials": {
+				"burning-stag-hide": 1200,
+				"leather-straps": 1200,
+				"copper-bar": 1200,
+				"ironwood": 1200
+			  },
+			  "img": "item/season12/burning-usurper/boots.png"
+			},
+			{
+			  "name": "Antlered Ring",
+			  "level": 25,
+			  "materials": {
+				"burning-stag-hide": 1200,
+				"hide": 1200,
+				"silk": 1200,
+				"black-iron": 1200
+			  },
+			  "img": "item/season12/burning-usurper/ring.png"
+			},
+			{
+			  "name": "Spiked Warhammer",
+			  "level": 25,
+			  "materials": {
+				"burning-stag-hide": 1200,
+				"wildfire": 1200,
+				"kingswood-oak": 1200,
+				"leather-straps": 1200
+			  },
+			  "img": "item/season12/burning-usurper/weapon.png"
+			}
+		  ]
+		},
+		{
+		  "setName": "Chilled Corsair's",
+		  "setMat": "Frost Crusted Abalone",
+		  "products": [
+			{
+			  "name": "Kerchief",
+			  "level": 5,
+			  "materials": {
+				"frost-crusted-abalone": 10,
+				"silk": 10,
+				"weirwood": 10
+			  },
+			  "img": "item/season12/chilled-corsair/head.png"
+			},
+			{
+			  "name": "Seacoat",
+			  "level": 5,
+			  "materials": {
+				"frost-crusted-abalone": 10,
+				"dragonglass": 10,
+				"black-iron": 10
+			  },
+			  "img": "item/season12/chilled-corsair/chest.png"
+			},
+			{
+			  "name": "Trousers",
+			  "level": 5,
+			  "materials": {
+				"frost-crusted-abalone": 10,
+				"milk-of-the-poppy": 10,
+				"wildfire": 10
+			  },
+			  "img": "item/season12/chilled-corsair/pants.png"
+			},
+			{
+			  "name": "Boots",
+			  "level": 5,
+			  "materials": {
+				"frost-crusted-abalone": 10,
+				"goldenheart-wood": 10,
+				"leather-straps": 10
+			  },
+			  "img": "item/season12/chilled-corsair/boots.png"
+			},
+			{
+			  "name": "Coral Ring",
+			  "level": 5,
+			  "materials": {
+				"frost-crusted-abalone": 10,
+				"kingswood-oak": 10,
+				"ironwood": 10
+			  },
+			  "img": "item/season12/chilled-corsair/ring.png"
+			},
+			{
+			  "name": "Goldenwood Bow",
+			  "level": 5,
+			  "materials": {
+				"frost-crusted-abalone": 10,
+				"copper-bar": 10,
+				"hide": 10
+			  },
+			  "img": "item/season12/chilled-corsair/weapon.png"
+			},
+
+			{
+			  "name": "Kerchief",
+			  "level": 10,
+			  "materials": {
+				"frost-crusted-abalone": 20,
+				"silk": 20,
+				"weirwood": 20
+			  },
+			  "img": "item/season12/chilled-corsair/head.png"
+			},
+			{
+			  "name": "Seacoat",
+			  "level": 10,
+			  "materials": {
+				"frost-crusted-abalone": 20,
+				"dragonglass": 20,
+				"black-iron": 20
+			  },
+			  "img": "item/season12/chilled-corsair/chest.png"
+			},
+			{
+			  "name": "Trousers",
+			  "level": 10,
+			  "materials": {
+				"frost-crusted-abalone": 20,
+				"milk-of-the-poppy": 20,
+				"wildfire": 20
+			  },
+			  "img": "item/season12/chilled-corsair/pants.png"
+			},
+			{
+			  "name": "Boots",
+			  "level": 10,
+			  "materials": {
+				"frost-crusted-abalone": 20,
+				"goldenheart-wood": 20,
+				"leather-straps": 20
+			  },
+			  "img": "item/season12/chilled-corsair/boots.png"
+			},
+			{
+			  "name": "Coral Ring",
+			  "level": 10,
+			  "materials": {
+				"frost-crusted-abalone": 20,
+				"kingswood-oak": 20,
+				"ironwood": 20
+			  },
+			  "img": "item/season12/chilled-corsair/ring.png"
+			},
+			{
+			  "name": "Goldenwood Bow",
+			  "level": 10,
+			  "materials": {
+				"frost-crusted-abalone": 20,
+				"copper-bar": 20,
+				"hide": 20
+			  },
+			  "img": "item/season12/chilled-corsair/weapon.png"
+			},
+
+			{
+			  "name": "Kerchief",
+			  "level": 15,
+			  "materials": {
+				"frost-crusted-abalone": 120,
+				"silk": 120,
+				"weirwood": 120
+			  },
+			  "img": "item/season12/chilled-corsair/head.png"
+			},
+			{
+			  "name": "Seacoat",
+			  "level": 15,
+			  "materials": {
+				"frost-crusted-abalone": 120,
+				"dragonglass": 120,
+				"black-iron": 120
+			  },
+			  "img": "item/season12/chilled-corsair/chest.png"
+			},
+			{
+			  "name": "Trousers",
+			  "level": 15,
+			  "materials": {
+				"frost-crusted-abalone": 120,
+				"milk-of-the-poppy": 120,
+				"wildfire": 120
+			  },
+			  "img": "item/season12/chilled-corsair/pants.png"
+			},
+			{
+			  "name": "Boots",
+			  "level": 15,
+			  "materials": {
+				"frost-crusted-abalone": 120,
+				"goldenheart-wood": 120,
+				"leather-straps": 120
+			  },
+			  "img": "item/season12/chilled-corsair/boots.png"
+			},
+			{
+			  "name": "Coral Ring",
+			  "level": 15,
+			  "materials": {
+				"frost-crusted-abalone": 120,
+				"kingswood-oak": 120,
+				"ironwood": 120
+			  },
+			  "img": "item/season12/chilled-corsair/ring.png"
+			},
+			{
+			  "name": "Goldenwood Bow",
+			  "level": 15,
+			  "materials": {
+				"frost-crusted-abalone": 120,
+				"copper-bar": 120,
+				"hide": 120
+			  },
+			  "img": "item/season12/chilled-corsair/weapon.png"
+			},
+
+			{
+			  "name": "Kerchief",
+			  "level": 20,
+			  "materials": {
+				"frost-crusted-abalone": 400,
+				"silk": 400,
+				"weirwood": 400,
+				"copper-bar": 400
+			  },
+			  "img": "item/season12/chilled-corsair/head.png"
+			},
+			{
+			  "name": "Seacoat",
+			  "level": 20,
+			  "materials": {
+				"frost-crusted-abalone": 400,
+				"dragonglass": 400,
+				"black-iron": 400,
+				"silk": 400
+			  },
+			  "img": "item/season12/chilled-corsair/chest.png"
+			},
+			{
+			  "name": "Trousers",
+			  "level": 20,
+			  "materials": {
+				"frost-crusted-abalone": 400,
+				"milk-of-the-poppy": 400,
+				"wildfire": 400,
+				"kingswood-oak": 400
+			  },
+			  "img": "item/season12/chilled-corsair/pants.png"
+			},
+			{
+			  "name": "Boots",
+			  "level": 20,
+			  "materials": {
+				"frost-crusted-abalone": 400,
+				"goldenheart-wood": 400,
+				"leather-straps": 400,
+				"milk-of-the-poppy": 400
+			  },
+			  "img": "item/season12/chilled-corsair/boots.png"
+			},
+			{
+			  "name": "Coral Ring",
+			  "level": 20,
+			  "materials": {
+				"frost-crusted-abalone": 400,
+				"kingswood-oak": 400,
+				"ironwood": 400,
+				"goldenheart-wood": 400
+			  },
+			  "img": "item/season12/chilled-corsair/ring.png"
+			},
+			{
+			  "name": "Goldenwood Bow",
+			  "level": 20,
+			  "materials": {
+				"frost-crusted-abalone": 400,
+				"copper-bar": 400,
+				"hide": 400,
+				"dragonglass": 400
+			  },
+			  "img": "item/season12/chilled-corsair/weapon.png"
+			},
+
+			{
+			  "name": "Kerchief",
+			  "level": 25,
+			  "materials": {
+				"frost-crusted-abalone": 1200,
+				"silk": 1200,
+				"weirwood": 1200,
+				"copper-bar": 1200
+			  },
+			  "img": "item/season12/chilled-corsair/head.png"
+			},
+			{
+			  "name": "Seacoat",
+			  "level": 25,
+			  "materials": {
+				"frost-crusted-abalone": 1200,
+				"dragonglass": 1200,
+				"black-iron": 1200,
+				"silk": 1200
+			  },
+			  "img": "item/season12/chilled-corsair/chest.png"
+			},
+			{
+			  "name": "Trousers",
+			  "level": 25,
+			  "materials": {
+				"frost-crusted-abalone": 1200,
+				"milk-of-the-poppy": 1200,
+				"wildfire": 1200,
+				"kingswood-oak": 1200
+			  },
+			  "img": "item/season12/chilled-corsair/pants.png"
+			},
+			{
+			  "name": "Boots",
+			  "level": 25,
+			  "materials": {
+				"frost-crusted-abalone": 1200,
+				"goldenheart-wood": 1200,
+				"leather-straps": 1200,
+				"milk-of-the-poppy": 1200
+			  },
+			  "img": "item/season12/chilled-corsair/boots.png"
+			},
+			{
+			  "name": "Coral Ring",
+			  "level": 25,
+			  "materials": {
+				"frost-crusted-abalone": 1200,
+				"kingswood-oak": 1200,
+				"ironwood": 1200,
+				"goldenheart-wood": 1200
+			  },
+			  "img": "item/season12/chilled-corsair/ring.png"
+			},
+			{
+			  "name": "Goldenwood Bow",
+			  "level": 25,
+			  "materials": {
+				"frost-crusted-abalone": 1200,
+				"copper-bar": 1200,
+				"hide": 1200,
+				"dragonglass": 1200
+			  },
+			  "img": "item/season12/chilled-corsair/weapon.png"
+			}
+		  ]
+		}
+	]
+};
