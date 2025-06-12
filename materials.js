0ed3059e9ac1e5d7e06f9a3d840178ed04bfdb01
MarksmanGotc <151929315+MarksmanGotc@users.noreply.github.com>
2025-06-13 00:04:53 +0300

    Update materials.js

--- materials.js
+++ materials.js
@@ -36,7 +36,7 @@ const materials = {
 			},
 			"milk-of-the-poppy": {
 				"img": "materials/icon_crafting_milk_of_the_poppy.webp",
-				"Original-name": "Milk of the poppy"
+				"Original-name": "Milk of the Poppy"
 			},
 			"silk": {
 				"img": "materials/icon_crafting_silk.webp",
@@ -50,6 +50,315 @@ const materials = {
 				"img": "materials/icon_crafting_wild_fire.webp",
 				"Original-name": "Wildfire"
 			}
+		}
+	},
+	1: {
+		season: 1,
+		mats: {
+			"abalone": {
+				"img": "materials/season1/abalone.png",
+				"Original-name": "Abalone"
+			},
+			"blackfyre-penny": {
+				"img": "materials/season1/blackfyre-penny.png",
+				"Original-name": "Blackfyre Penny"
+			},
+			"bone": {
+				"img": "materials/season1/bone.png",
+				"Original-name": "Bone"
+			},
+			"driftwood": {
+				"img": "materials/season1/driftwood.png",
+				"Original-name": "Driftwood"
+			},
+			"golden-claw": {
+				"img": "materials/season1/golden-claw.png",
+				"Original-name": "Golden Claw"
+			},
+			"horse-hide": {
+				"img": "materials/season1/horse-hide.png",
+				"Original-name": "Horse Hide"
+			},
+			"lock-of-hair": {
+				"img": "materials/season1/lock-of-hair.png",
+				"Original-name": "Lock of Hair"
+			},
+			"pinecone": {
+				"img": "materials/season1/pinecone.png",
+				"Original-name": "Pinecone"
+			},
+			"qartheen-jeweled-beetle": {
+				"img": "materials/season1/qartheen-jeweled-beetle.png",
+				"Original-name": "Qartheen Jeweled Beetle"
+			},
+			"raven-feather": {
+				"img": "materials/season1/raven-feather.png",
+				"Original-name": "Raven Feather"
+			}
+		}
+	},	
+	2: {
+		season: 2,
+		mats: {
+			"bucket-of-tar": {
+				"img": "materials/season2/bucket-of-tar.png",
+				"Original-name": "Bucket of Tar"
+			},
+			"cockle-shell": {
+				"img": "materials/season2/cockle-shell.png",
+				"Original-name": "Cockle Shell"
+			},
+			"dragon-spine": {
+				"img": "materials/season2/dragon-spine.png",
+				"Original-name": "Dragon Spine"
+			},
+			"golden-thread": {
+				"img": "materials/season2/golden-thread.png",
+				"Original-name": "Golden Thread"
+			},
+			"hoof": {
+				"img": "materials/season2/hoof.png",
+				"Original-name": "Hoof"
+			},
+			"horsehair-tassel": {
+				"img": "materials/season2/horsehair-tassel.png",
+				"Original-name": "Horsehair Tassel"
+			},
+			"horsehoe": {
+				"img": "materials/season2/horsehoe.png",
+				"Original-name": "Horsehoe"
+			}
+		}
+	},
+	3: {
+		season: 3,
+		mats: {
+			"ancient-leather": {
+				"img": "materials/season3/ancient-leather.png",
+				"Original-name": "Ancient Leather"
+			},
+			"battered-ringmail": {
+				"img": "materials/season3/battered-ringmail.png",
+				"Original-name": "Battered Ringmail"
+			},
+			"black-leather": {
+				"img": "materials/season3/black-leather.png",
+				"Original-name": "Black Leather"
+			},
+			"conqueror-shagreen": {
+				"img": "materials/season3/conqueror-shagreen.png",
+				"Original-name": "Conqueror's Shagreen"
+			},
+			"floral-silk": {
+				"img": "materials/season3/floral-silk.png",
+				"Original-name": "Floral Silk"
+			},
+			"kingsguard-emblems": {
+				"img": "materials/season3/kingsguard-emblems.png",
+				"Original-name": "Kingsguard Emblems"
+			},
+			"silvered-steel": {
+				"img": "materials/season3/silvered-steel.png",
+				"Original-name": "Silvered Steel"
+			}
+		}
+	},
+	4: {
+		season: 4,
+		mats: {
+			"black-scalemail": {
+				"img": "materials/season4/black-scalemail.png",
+				"Original-name": "Black Scalemail"
+			},
+			"cloth-of-silver": {
+				"img": "materials/season4/cloth-of-silver.png",
+				"Original-name": "Cloth of Silver"
+			},
+			"composite-cloth": {
+				"img": "materials/season4/composite-cloth.png",
+				"Original-name": "Composite Cloth"
+			},
+			"gilded-plates": {
+				"img": "materials/season4/gilded-plates.png",
+				"Original-name": "Gilded Plates"
+			},
+			"heavy-brocade": {
+				"img": "materials/season4/heavy-brocade.png",
+				"Original-name": "Heavy Brocade"
+			},
+			"leather-and-fletchings": {
+				"img": "materials/season4/leather-and-fletchings.png",
+				"Original-name": "Leather and Fletchings"
+			},
+			"stag-hide": {
+				"img": "materials/season4/stag-hide.png",
+				"Original-name": "Stag Hide"
+			}
+		}
+	},
+	5: {
+		season: 5,
+		mats: {
+			"crimson-taffeta": {
+				"img": "materials/season5/crimson-taffeta.png",
+				"Original-name": "Crimson Taffeta"
+			},
+			"green-chiffon": {
+				"img": "materials/season5/green-chiffon.png",
+				"Original-name": "Green Chiffon"
+			},
+			"pilfered-boots": {
+				"img": "materials/season5/pilfered-boots.png",
+				"Original-name": "Pilfered Boots"
+			},
+			"red-clay-tiles": {
+				"img": "materials/season5/red-clay-tiles.png",
+				"Original-name": "Red Clay Tiles"
+			},
+			"royal-samite": {
+				"img": "materials/season5/royal-samite.png",
+				"Original-name": "Royal Samite"
+			},
+			"scavenged-bone": {
+				"img": "materials/season5/scavenged-bone.png",
+				"Original-name": "Scavenged Bone"
+			},
+			"stitched-leather": {
+				"img": "materials/season5/stitched-leather.png",
+				"Original-name": "Stitched Leather"
+			}
+		}
+	},
+	6: {
+		season: 6,
+		mats: {
+			"alligator-skins": {
+				"img": "materials/season6/alligator-skins.png",
+				"Original-name": "Alligator Skins"
+			},
+			"hardened-leather": {
+				"img": "materials/season6/hardened-leather.png",
+				"Original-name": "Hardened Leather"
+			},
+			"lavish-silk": {
+				"img": "materials/season6/lavish-silk.png",
+				"Original-name": "Lavish Silk"
+			},
+			"rimed-leather": {
+				"img": "materials/season6/rimed-leather.png",
+				"Original-name": "Rimed Leather"
+			},
+			"royal-velvet": {
+				"img": "materials/season6/royal-velvet.png",
+				"Original-name": "Royal Velvet"
+			},
+			"seawater-cloth": {
+				"img": "materials/season6/seawater-cloth.png",
+				"Original-name": "Seawater Cloth"
+			},
+			"weirwood-sap": {
+				"img": "materials/season6/weirwood-sap.png",
+				"Original-name": "Weirwood Sap"
+			}
+		}
+	},
+	7: {
+		season: 7,
+		mats: {
+			"boiled-leather": {
+				"img": "materials/season7/boiled-leather.png",
+				"Original-name": "Boiled Leather"
+			},
+			"drowned-leather": {
+				"img": "materials/season7/drowned-leather.png",
+				"Original-name": "Drowned Leather"
+			},
+			"fur-lined-leather": {
+				"img": "materials/season7/fur-lined-leather.png",
+				"Original-name": "Fur-Lined Leather"
+			},
+			"linked-mail": {
+				"img": "materials/season7/linked-mail.png",
+				"Original-name": "Linked Mail"
+			},
+			"luxurious-linen": {
+				"img": "materials/season7/luxurious-linen.png",
+				"Original-name": "Luxurious Linen"
+			},
+			"sundry-steel": {
+				"img": "materials/season7/sundry-steel.png",
+				"Original-name": "Sundry Steel"
+			},
+			"untarnished-leather": {
+				"img": "materials/season7/untarnished-leather.png",
+				"Original-name": "Untarnished Leather"
+			}
+		}
+	},
+	8: {
+		season: 8,
+		mats: {
+			"dark-steel-plate": {
+				"img": "materials/season8/dark-steel-plate.png",
+				"Original-name": "Dark Steel Plate"
+			},
+			"dragon-tempered": {
+				"img": "materials/season8/dragon-tempered.png",
+				"Original-name": "Dragon Tempered Fabrics"
+			},
+			"gilded-mail": {
+				"img": "materials/season8/gilded-mail.png",
+				"Original-name": "Gilded Mail"
+			},
+			"northern-fox-pelts": {
+				"img": "materials/season8/northern-fox-pelts.png",
+				"Original-name": "Northern Fox Pelts"
+			},
+			"ritual-cloths": {
+				"img": "materials/season8/ritual-cloths.png",
+				"Original-name": "Ritual Cloths"
+			},
+			"silk-brocade": {
+				"img": "materials/season8/silk-brocade.png",
+				"Original-name": "Silk Brocade"
+			},
+			"targaryen-embroidery": {
+				"img": "materials/season8/targaryen-embroidery.png",
+				"Original-name": "Targaryen Embroidery"
+			},
+		}
+	},
+	9: {
+		season: 9,
+		mats: {
+			"bronze-discs": {
+				"img": "materials/season9/bronze-discs.png",
+				"Original-name": "Bronze Discs"
+			},
+			"hardy-silks": {
+				"img": "materials/season9/hardy-silks.png",
+				"Original-name": "Hardy Silks"
+			},
+			"kingsguard-leather": {
+				"img": "materials/season9/kingsguard-leather.png",
+				"Original-name": "Kingsguard Leather"
+			},
+			"liberator-silk": {
+				"img": "materials/season9/liberator-silk.png",
+				"Original-name": "Liberator's Silk"
+			},
+			"motley-cloth": {
+				"img": "materials/season9/motley-cloth.png",
+				"Original-name": "Motley Cloth"
+			},
+			"northern-leathers": {
+				"img": "materials/season9/northern-leathers.png",
+				"Original-name": "Northern Leathers"
+			},
+			"steel-scales": {
+				"img": "materials/season9/steel-scales.png",
+				"Original-name": "Steel Scales"
+			},
 		}
 	},
 	10: {
@@ -115,7 +424,7 @@ const materials = {
 			"targaryen-cloth-bolt": {
 				"img": "materials/season11/targaryen-cloth-bolt.png",
 				"Original-name": "Targaryen Cloth Bolt"
-			},
+			}
 		}
 	},
 	12: {
