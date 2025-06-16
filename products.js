674464e81ed2851ed8ae0a35b309048fc94131a8
MarksmanGotc <151929315+MarksmanGotc@users.noreply.github.com>
2025-06-16 23:04:53 +0300

    Update products.js

--- products.js
+++ products.js
@@ -7,6 +7,13 @@ let craftItem = {
 						season: season0.season
 				}))
 		),
+		...season8.sets.flatMap(set =>
+				set.products.map(product => ({
+						...product,
+						setName: product.setName || set.setName,
+						season: season8.season
+				}))
+		),
 		...season9.sets.flatMap(set =>
 				set.products.map(product => ({
 						...product,
@@ -38,4 +45,4 @@ let craftItem = {
 	]
 };
 
-window.seasons = [season0, season9, season10, season11, season12];
+window.seasons = [season0, season8, season9, season10, season11, season12];
